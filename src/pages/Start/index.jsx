18636de7968c7cbe38a25821aa1069cb58c590a2
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-11-29 00:29:54 -0300

    inserindo botão padrão com estilo

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -1,26 +1,39 @@
 import React from "react";
 import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
+import DefaultButton from "../../components/Common/DefaultButton";
 import LifeStatus from "../../components/Common/LifeStatus/";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Start() {
-    return (
-        <View style={styles.container}>
-          <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
-            <View style={{ alignItems: "center" }}>
-              <Image
-                source={require("../../assets/icons/logo3.png")}
-                style={styles.logo}
-              />
-              <LifeStatus/>
-              <Text style={styles.description}>
-                Vamos transformar sua vida {"\n"} 
-                em um jogo, buscando sempre {"\n"}{" "}
-                o melhor nível.
-              </Text>
-            </View>
-          </ScrollView>
-        </View>
-      );
+
+  const handleNavAppExplanation = ()=>{
+    console.log("testando click")
+  }
+
+  return (
+      <View style={styles.container}>
+        <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              source={require("../../assets/icons/logo3.png")}
+              style={styles.logo}
+            />
+            <LifeStatus/>
+            <Text style={styles.description}>
+              Vamos transformar sua vida {"\n"} 
+              em um jogo, buscando sempre {"\n"}{" "}
+              o melhor nível.
+            </Text>
+            <DefaultButton 
+              buttonText={"Continuar..."}
+              handlePress={handleNavAppExplanation}
+              width={250}
+              height={50}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
